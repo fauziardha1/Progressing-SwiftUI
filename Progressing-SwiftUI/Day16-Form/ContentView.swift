e1e418f02bd8103ca61124b349b820fd541453be
fauziardha1 <fauziardasaputra@gmail.com>
2022-08-26 02:38:36 +0700

    test

--- Progressing-SwiftUI/Day16-Form/ContentView.swift
+++ Progressing-SwiftUI/Day16-Form/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
                     Text("Hello Word!")
                     Text("Hello Word!")
                     Text("Hello Word!")
-                    Text("Hello Word!")
+                    Text("Hello Word!w")
                 }
                 Section{
                     Text("Hello Word!")
